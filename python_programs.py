fd8ed66c2f08af415bc36d07a17bac8e78cf552e
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-12-24 19:10:02 +0000

    feat: Implemeted python programiz 2b

--- python_programs.py
+++ python_programs.py
@@ -1,9 +1,9 @@
 # Python Program to Add Two Numbers
 
-num1 = 1.5
-num2 = 6.3
+num1 = input('Enter first number: ')
+num2 = input('Enter second number: ')
 
-sum = num1 + num2
+sum = float(num1) + float(num2)
 
 print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
 
