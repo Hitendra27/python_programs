292c2217fb8389e5d2f2a86c9b3e0a5e98422806
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-12-25 10:36:31 +0000

    feat: Implemented pyton programiz 3

--- python_programs.py
+++ python_programs.py
@@ -1,9 +1,6 @@
-# Python Program to Add Two Numbers
+# Python Program to Find the Square Root
 
-num1 = input('Enter first number: ')
-num2 = input('Enter second number: ')
+num = 8
 
-sum = float(num1) + float(num2)
-
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-
+num_sqrt = num ** 0.5
+print('The square root of %0.3f is %0.3f'%(num , num_sqrt))
