66e51e21e0608d04f93136947bdf895d3f624bf5
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-12-28 18:37:08 +0000

    feat: Implemented python programiz 10b

--- python_programs.py
+++ python_programs.py
@@ -1,11 +1,11 @@
 # Python Program to Check if a Number is Positive, Negative or 0
 
-num = -4
+num = 12
 
-if num > 0:
-    print("Positive number")
-elif num == 0:
-    print("Zero")
-else:
-    print("Negative number")
-
+if num >= 0:
+    if num == 0:
+        print("Zero")
+    else:
+        print("Positive number")
+else: 
+   print("Negative number")
