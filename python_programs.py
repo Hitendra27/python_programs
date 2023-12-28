a39abafb57ba62efddb1547871c098ff206f7b95
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-12-28 12:49:42 +0000

    feat: Implemented python programiz 9a

--- python_programs.py
+++ python_programs.py
@@ -1,11 +1,9 @@
-# Python Program to Convert Kilometers to Miles
+# Python Program to Convert Celsius To Fahrenheit
 
-Kilometers = 3.5
+celsius = 37.5
 
-conv_fac = 0.621371
-
-miles = Kilometers * conv_fac
-print('%0.2f kilometers is equal to %0.2f miles' %(Kilometers,miles))
+fahrenheit = (celsius * 1.8) + 32
+print('%0.1f degree Celsius is qual to %0.1f degree Fahrenheit' %(celsius,fahrenheit))
 
 
 
