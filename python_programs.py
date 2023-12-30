2e73664e8e9772189181dde3cec5562fd4b6e331
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-12-30 19:10:22 +0000

    feat: Implemented python programiz 14b

--- python_programs.py
+++ python_programs.py
@@ -1,19 +1,19 @@
 # Python Program to Check Prime Number
 
-num = 29
-
-flag = False
+num = 407
 
 if num == 1:
     print(num, "is not a prime number")
 elif num > 1:
     for i in range(2, num):
         if (num % i) == 0:
-            flag = True
+            print(num, "is not a prime number")
+            print(i,"times",num//i,"is",num)
             break
 
-        if flag: 
-            print(num, "is not a prime number")
-        else:
-            print(num, "is a prime number")
+    else:
+        print(num, "is a prime number")
+
+else: 
+    print(num,"is not a prime number")
 
