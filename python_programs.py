8dfd3b853ea2eba54052c665aac558e66cc346a8
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2023-12-31 18:38:35 +0000

    feat: Implemented python programiz 15

--- python_programs.py
+++ python_programs.py
@@ -1,19 +1,14 @@
-# Python Program to Check Prime Number
+# Python Program to Print all Prime Numbers in an Interval
 
-num = 407
+lower = 900
+upper = 1000
 
-if num == 1:
-    print(num, "is not a prime number")
-elif num > 1:
-    for i in range(2, num):
-        if (num % i) == 0:
-            print(num, "is not a prime number")
-            print(i,"times",num//i,"is",num)
-            break
+print("Prime numbers between", lower, "and", upper, "are:")
 
-    else:
-        print(num, "is a prime number")
-
-else: 
-    print(num,"is not a prime number")
-
+for num in range(lower, upper + 1):
+    if num > 1:
+        for i in range(2, num):
+            if (num % i) == 0:
+                break
+        else:
+            print(num)
