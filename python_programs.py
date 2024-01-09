0ab78aa1c04d5f0b402b7d357c940510fe9cab18
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-01-09 12:09:53 +0000

    feat: Implemented pyhton programiz 18

--- python_programs.py
+++ python_programs.py
@@ -1,6 +1,18 @@
-# Python Program to Display the multiplication Table
+# Python Program to Print the Fibonacci sequence
 
-num = 12
+nterms = 7
 
-for i in range(1, 11):
-    print(num, 'x', i, '=', num * i)
+n1, n2 = 0, 1
+count = 0
+
+if nterms == 1:
+    print("Fibonacci sequence upto",nterms,".")
+    print(n1)
+else:
+    print("Fibonacci sequence:")
+    while count < nterms:
+        print(n1)
+        nth = n1 + n2
+        n1 = n2
+        n2 = nth
+        count += 1
